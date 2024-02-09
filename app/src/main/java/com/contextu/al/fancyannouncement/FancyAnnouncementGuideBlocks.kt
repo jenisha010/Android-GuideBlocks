2dcc1bbd5eb3d91ef919ed95cb5486c6b512e8d6
Marc Stroebel <mdstroebel@gmail.com>
2024-02-09 16:52:04 +1100

    fix: change FancyAnnouncement to subclass Dialog

--- app/src/main/java/com/contextu/al/fancyannouncement/FancyAnnouncementGuideBlocks.kt
+++ app/src/main/java/com/contextu/al/fancyannouncement/FancyAnnouncementGuideBlocks.kt
@@ -10,21 +10,29 @@ import com.bumptech.glide.Glide
 import com.contextu.al.R
 
 
-class FancyAnnouncementGuideBlocks(private val activity: Activity) {
+class FancyAnnouncementGuideBlocks(private val activity: Activity): Dialog(activity) {
 
-    fun show(title: String, content: String, negativeText: String, negativeButtonListener: View.OnClickListener,
-             positiveText: String, positiveButtonListener: View.OnClickListener, imageUrl: String){
+    fun show(title: String,
+             content: String,
+             negativeText: String,
+             negativeButtonListener: View.OnClickListener,
+             positiveText: String,
+             positiveButtonListener: View.OnClickListener,
+             imageUrl: String) {
         val dialog = Dialog(activity)
         dialog.setContentView(R.layout.fancy_announcement)
         dialog.window?.setLayout((activity.resources.displayMetrics.widthPixels * 0.90).toInt(),
             (activity.resources.displayMetrics.heightPixels * 0.55).toInt())
         dialog.show()
+
         val fancyAnnouncementImage = dialog.findViewById<ImageView>(R.id.announcementImage)
         fancyAnnouncementImage?.let {
             Glide.with(activity.baseContext).load(imageUrl).into(fancyAnnouncementImage)
         }
+
         val fancyAnnouncementTitle = dialog.findViewById<TextView>(R.id.title)
         fancyAnnouncementTitle?.text = title
+
         val fancyAnnouncementContent = dialog.findViewById<TextView>(R.id.content)
         fancyAnnouncementContent?.text = content
 
@@ -35,6 +43,5 @@ class FancyAnnouncementGuideBlocks(private val activity: Activity) {
         val cancelButton = dialog.findViewById<Button>(R.id.cancel_button)
         cancelButton?.text = negativeText
         cancelButton.setOnClickListener(negativeButtonListener)
-
     }
 }
