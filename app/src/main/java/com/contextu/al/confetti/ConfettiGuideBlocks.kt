a021552243d50c3f9424232630f886050210186f
Marc Stroebel <mdstroebel@gmail.com>
2024-02-13 16:13:22 +1100

    fix: revert activity set content view

--- app/src/main/java/com/contextu/al/confetti/ConfettiGuideBlocks.kt
+++ app/src/main/java/com/contextu/al/confetti/ConfettiGuideBlocks.kt
@@ -28,8 +28,6 @@ class ConfettiGuideBlocks(private val activity: Activity){
         val viewKonfetti = view.findViewById<KonfettiView>(R.id.konfettiView)
         viewGroup.addView(viewKonfetti)
 
-        activity.setContentView(viewKonfetti)
-
         viewKonfetti.start(party)
 
         viewKonfetti.onParticleSystemUpdateListener = object : OnParticleSystemUpdateListener{
